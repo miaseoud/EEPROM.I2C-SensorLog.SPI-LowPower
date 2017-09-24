715af522125e510d36be58535b05c59df4c1f16f
Mahmoud Aboul-Seoud <m.aboulseoud@tum.de>
2017-09-25 01:21:21 +0200

    SPI Sensor readings from MISO

--- AVR_SPI_UART.c
+++ AVR_SPI_UART.c
@@ -10,6 +10,11 @@
 #define F_CPU 4000000UL
 #define FOSC 4000000
 #define BAUD 9600
+#define DDR_SPI DDRB
+#define DD_MOSI 5
+//#define DD_MISO 6
+#define DD_SCK 7
+#define DD_SS 4
 void UART_init (){
 	/*UCSRA,B,C are initialized as 0x00 */
 	UBRRL= (uint8_t) (FOSC/16/BAUD-1); //low 8 bits of the UBRR
@@ -26,11 +31,25 @@ uint8_t UART_Read(){
     while( !(UCSRA & (1<<7)) ) ;//wait until the receiving complete RXCn
 	return UDR;
 }
+
+void SPI_MasterInit(void){
+/* Set MOSI and SCK output, all others input */
+DDR_SPI = (1<<DD_MOSI)|(1<<DD_SCK)|(1<<DD_SS) ;
+SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0); ///Enable SPI, Set as Master, clock rate fck/16 
+}
+void SPI_MasterTransmit(char cData){
+SPDR = cData;/* Start transmission */
+while(!(SPSR & (1<<SPIF)));// Wait for transmission to complete: When a serial transfer is complete, the SPIF Flag is set
+}
+
 int main(void){
+	int val;
 	DDRA=0xFF;
 	PORTA=0x00;
     UART_init();
-	UART_Transmit(99);
+	PORTB |= 1<<4;
+	SPI_MasterInit();
+	UART_Transmit(99);	
     while(1) {
       if(UART_Read()=='0'){
 	  PORTA=0x00;
@@ -40,5 +59,12 @@ int main(void){
 	  PORTA=0xFF;
 	  UART_Transmit(98);
 	  }
+	  PORTB &= ~(1<<4);
+	  SPI_MasterTransmit(0);
+	  val=SPDR<<5;
+	  SPI_MasterTransmit(0);
+	  PORTB |= 1<<4;
+	  val=val | SPDR>>3;
+	  UART_Transmit(val*0.0625);
     }
 }
